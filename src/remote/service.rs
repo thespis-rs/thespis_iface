d48af2d06ad6e79c3cf3c8b49ba2df1bdc4b5783
Naja Melan <najamelan@autistici.org>
2019-04-19 22:51:02 +0200

    Remove Service::sid in favor of Service::uid

--- src/remote/service.rs
+++ src/remote/service.rs
@@ -8,9 +8,6 @@ pub trait Service
 {
 	type UniqueID: UniqueID;
 
-	fn sid() -> Self::UniqueID;
-
-
 	fn uid( seed: &[u8] ) -> Self::UniqueID;
 
 	//fn recipient( peer: impl Address<impl Router> ) -> Recipient<Self> ;
