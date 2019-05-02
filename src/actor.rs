a9173120fd22308da4507c0cf215ab0edec8241e
Naja Melan <najamelan@autistici.org>
2019-05-02 13:04:27 +0200

    Get rid of .boxed() from futures-rs because they added a Send bound to the resulting box

--- src/actor.rs
+++ src/actor.rs
@@ -1,4 +1,4 @@
-use crate :: { *, import::* };
+use crate :: { * };
 
 
 /// An actor is an isolated computing unit. For an introduction to the actor model, see:
@@ -9,11 +9,11 @@ pub trait Actor: 'static
 	/// Gets called just before the mailbox starts listening for incoming messages.
 	/// You can do this to do setup for your actor.
 	//
-	fn started ( &mut self ) -> Return<()> { async {}.boxed() }
+	fn started ( &mut self ) -> Return<()> { Box::pin( async {} ) }
 
 	/// Gets called just after the mailbox stops listening for messages.
 	/// You can use this to do cleanup.
 	//
-	fn stopped ( &mut self ) -> Return<()> { async {}.boxed() }
+	fn stopped ( &mut self ) -> Return<()> { Box::pin( async {} ) }
 }
 
--- src/remote/multi_service.rs
+++ src/remote/multi_service.rs
@@ -3,17 +3,14 @@ use crate::{ import::*, * } ;
 
 /// Represents a message as it is over the wire, with some metadata attached to the serialized `Message`.
 ///
-/// The actual wire format is not defined here, but is implementation dependent. This seems bad for intorp,
-/// but it is actually not conceivalbe right now to define a single format and force that on all users.
+/// The actual wire format is not defined here, but is implementation dependent. This seems bad for interop,
+/// but it is actually not conceivalbe right now to define a single format and force that on all users. We
+/// might actually want to implement a cap'n proto version to make it more compatible with other languages.
 ///
 /// Different tradeoffs might require different layout. CPU vs Memory consumption for example.
 /// Maybe a few default formats could be suggested later. Like a "compact" format and a "fast" format.
 ///
-/// The trait bound `for<'a> TryFrom< &'a mut BytesMut >` is for codecs like Tokio Codec. You should
-/// implement this as you would implement Decoder::decode. Advancing the pointer and taking possesion
-/// of the data that successfully decoded once an entire frame is present.
-///
-/// In contrast, the into and try_from Bytes are not mutable, and implementations are supposed to just
+/// The Into and From Bytes are not mutable, implementations are supposed to just
 /// wrap them without copying anything. Providing a view into the memory through the trait methods.
 //
 pub trait MultiService : Into< Bytes > + From< Bytes > + Send + Sync
@@ -22,9 +19,41 @@ pub trait MultiService : Into< Bytes > + From< Bytes > + Send + Sync
 	type ConnID   : UniqueID;
 	type CodecAlg : CodecAlg;
 
-	fn len     ( &self ) -> usize                            ;
+	/// Create a new MultiService. I don't like to put constructors in a trait, but for now we need it in
+	/// our impl of Peer and service_map_macro.
+	//
+	fn create
+	(
+		service : Self::ServiceID ,
+		conn_id : Self::ConnID    ,
+		encoding: Self::CodecAlg  ,
+		mesg    : Bytes           ,
+
+	) -> Self
+
+		where Self: Sized
+	;
+
+	/// The total length of the Multiservice in Bytes (header+payload)
+	//
+	fn len( &self ) -> usize;
+
+	/// The service id of this message. When coming in over the wire, this identifies
+	/// which service you are calling. A ServiceID should be unique for a given service.
+	/// The reference implementation combines a unique type id with a namespace so that
+	/// several processes can accept the same type of service under a unique name each.
+	//
 	fn service ( &self ) -> Result< Self::ServiceID, Error > ;
+
+	/// The connection id. This is used to match responses to outgoing calls.
+	//
 	fn conn_id ( &self ) -> Result< Self::ConnID   , Error > ;
+
+	/// The serialization codec for the payload.
+	//
 	fn encoding( &self ) -> Result< Self::CodecAlg , Error > ;
-	fn mesg    ( &self ) -> Bytes                            ;
+
+	/// The serialized payload message.
+	//
+	fn mesg( &self ) -> Bytes;
 }
